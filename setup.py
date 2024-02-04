ee961f9cc88f07182a97d3fff9ac7cf1039e624b
Matthias Koeppe <mkoeppe@math.ucdavis.edu>
2024-02-03 18:31:12 -0800

    pyproject.toml: Move metadata here from setup.py

--- setup.py
+++ setup.py
@@ -13,9 +13,6 @@ class build_ext(du_build_ext):
         du_build_ext.run(self)
 
 
-with open("README.md", "r", encoding="utf-8") as fh:
-    long_description = fh.read()
-
 with open('VERSION') as version_file:
     version = version_file.read().strip()
 
@@ -30,33 +27,13 @@ extensions = [
     ]
 
 setup(
-    name='primecountpy',
-    description='Cython interface for C++ primecount library',
     version=version,
-    long_description=long_description,
-    long_description_content_type='text/markdown',
-    url='https://github.com/dimpase/primecountpy',
-    author='SageMath Developers',
-    author_email='sage-devel@googlegroups.com',
-    project_urls={
-        "User Manual": "https://primecountpy.readthedocs.io",
-        "Bug Tracker": "https://github.com/dimpase/primecountpy/issues",
-    },
-    license='GPLv3',
     packages=find_packages(),
     ext_modules=extensions,
     zip_safe=False,
-    python_requires='>=3.7',
     package_dir={'primecountpy': 'primecountpy'},
-    install_requires=["cysignals"],
     package_data={"primecountpy": ["*.pxd"],
           "docs": ["*"], "docs.source": ["*"], "docs.source.modules": ["*"],
           },
     cmdclass={'build_ext': build_ext},
-    classifiers=[
-        'Development Status :: 2 - Pre-Alpha',
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: GNU General Public License v3 (GPLv3)',
-        'Programming Language :: Python :: 3',
-        'Topic :: Scientific/Engineering :: Mathematics']
-    )
+)
