8d1fb6892bd470f85d8d145a11aad60267b9b631
Dima Pasechnik <dima@pasechnik.info>
2025-05-07 22:50:11 -0500

    1st working version

--- primecount.cpp
+++ primecount.cpp
@@ -1,14 +1,15 @@
 #include <nanobind/nanobind.h>
 #include <primecount.hpp>
+#include <string>
 
 namespace nb = nanobind;
 
-template <typename T> pi_t(T& x) {
+template <typename T, typename R> R pit(T& x) {
    return primecount::pi(x);
 }
 
-NB_MODULE(primecount, m) {
-    m.def("pi", &<std::string>pi_t);
-    m.def("pi", &<int64_t>pi_t);
+NB_MODULE(primecountpy, m) {
+    m.def("pi", &pit<std::string&, std::string>);
+    m.def("pi", &pit<int64_t, int64_t>);
 }
 
